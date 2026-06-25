4a76ff2078bfc45db0ed9d9403b8c0db52f28ca9
Griss <grissrll@gmail.com>
2026-06-25 12:57:31 +0300

    CREATE SHEMAS FOR USER
    	BaseUserSchema
    	CreateUserSchema
    	UpdateUserSchema
    	UserResponseSchema

---
+++ app/schemas/users.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, EmailStr, Field
+
+
+class BaseUserSchema(BaseModel):
+    email: EmailStr = Field(min_length=10, description="Enter your email.")
+    user_name: str = Field(min_length=3,max_length=40, description="Enter your name.")
+
+class CreateUserSchema(BaseUserSchema):
+    pass
+
+class UpdateUserSchema(BaseModel):
+    email: EmailStr | None = None
+    user_name: str | None = Field(min_length=3,
+                                  max_length=40,
+                                  default=None,
+                                  description="Enter your name")
+
+class UserResponseSchema(BaseUserSchema):
+    id: int
+    model_config = {"from_attributes": True}
+
+
+
