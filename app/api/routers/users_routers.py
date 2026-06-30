1e5c738fcc7c67303cbefdc4cade2df97e499d47
Griss <grissrll@gmail.com>
2026-06-30 12:02:17 +0300

    ADD ENDPOINT get_user_by_id IN routers/users_routers.py
    	crt endpoint -> get_user_by_id for retrieving user by id
    	add tests for endpoint:
    		test_get_user_by_id_422
    		test_get_user_by_id_200

--- app/api/routers/users_routers.py
+++ app/api/routers/users_routers.py
@@ -4,7 +4,7 @@ from sqlalchemy.util import await_only
 from app.core.depends import get_db
 from app.models.users import User as UserModel
 from app.schemas.users import UserResponseSchema, CreateUserSchema
-from app.services.users_services import get_all_users_service, create_user_services
+from app.services.users_services import get_all_users_service, create_user_services, get_user_by_id_service
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from typing import List
@@ -25,3 +25,8 @@ async def get_all_users(db: AsyncSession = Depends(get_db),
 @router.post("/", status_code=201, response_model=UserResponseSchema)
 async def create_new_user(user: CreateUserSchema, db: AsyncSession = Depends(get_db)):
     return await create_user_services(db, user)
+
+
+@router.get("/{user_id}", status_code=200, response_model=UserResponseSchema)
+async def get_user_by_id(user_id: int, db: AsyncSession = Depends(get_db)):
+    return await get_user_by_id_service(db, user_id)
--- app/tests/api/test_users_routers.py
+++ app/tests/api/test_users_routers.py
@@ -43,7 +43,7 @@ async def test_get_all_users_200_with_users(client, async_session_maker):
 
 
 @pytest.mark.asyncio
-async def test_create_new_user_201(client, async_session_maker):
+async def test_create_new_user_201(client):
     response = await client.post('/users/', json=user_data_ok)
     resp_data = response.json()
     assert resp_data is not None
@@ -52,6 +52,22 @@ async def test_create_new_user_201(client, async_session_maker):
 
 
 @pytest.mark.asyncio
-async def test_create_new_user_422(client, async_session_maker):
+async def test_create_new_user_422(client):
     response = await client.post('/users/', json=user_data_wrong)
+    assert response.status_code == 422
+
+@pytest.mark.asyncio
+async def test_get_user_by_id_200(client, async_session_maker):
+    async with async_session_maker() as session:
+        await session.execute(insert(UserModel), user_data_ok)
+        await session.commit()
+        await session.close()
+    response = await client.get("/users/1")
+    assert response is not None
+    assert response.status_code == 200
+    assert response.json()["user_name"] == user_data_ok["user_name"]
+
+@pytest.mark.asyncio
+async def test_get_user_by_id_422(client):
+    response = await client.get('/users/dac')
     assert response.status_code == 422
