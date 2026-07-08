aee8773df9c5c694a7c284e63209b04112bf1193
Griss <grissrll@gmail.com>
2026-07-08 12:53:35 +0300

    refactor: add constraint for title in projects
    	- change unique status for field title
    	- with unique_constraints and give name for title
    	- make migration for that

--- app/models/projects.py
+++ app/models/projects.py
@@ -1,5 +1,5 @@
 from .base import Base
-from sqlalchemy import Boolean, Integer, String, ForeignKey, DateTime, Text, func
+from sqlalchemy import Boolean, Integer, String, ForeignKey, DateTime, UniqueConstraint, Text, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 from typing import List
@@ -9,7 +9,7 @@ class Project(Base):
     __tablename__ = "projects"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(120), nullable=False)
     description: Mapped[str | None] = mapped_column(Text, nullable=True)
     owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=True)
@@ -19,6 +19,8 @@ class Project(Base):
     finished_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
 
     owner: Mapped['User'] = relationship("User", back_populates="projects")
-    tasks: Mapped[List['Task']] = relationship("Task", back_populates="project",cascade="all, delete-orphan")
+    tasks: Mapped[List['Task']] = relationship("Task", back_populates="project", cascade="all, delete-orphan")
 
-
+    __table_args__ = (
+        UniqueConstraint("title", name="un_projects_title"),
+    )
